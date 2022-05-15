f5b48a90c6c3effe35ebea11b30b5ddf41c8aa8b
denilson <denilson.silvastt@gmail.com>
2022-05-15 01:40:21 -0300

    header commit v1

--- src/pages/Home/components/header.js
+++ src/pages/Home/components/header.js
@@ -1,19 +1,59 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { makeStyles } from "@material-ui/styles";
+import { AppBar } from "@mui/material";
+import { Toolbar } from "@mui/material";
+import { SvgIcon } from "@mui/material";
+import { Bell } from "react-feather";
+import { Avatar } from "@mui/material";
+
+//erro boxShadow
+const useStyles = makeStyles({
+    appBar:{
+        
+    },
+    img: {
+        maxHeight: 55
+    },
+    grow: {
+        flexGrow: 1
+    },
+    userSection: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    bell: {
+        marginRight: 10,
+        marginLeft: 10
+    }
+});
 
 function Header() {
+    const classes = useStyles();
     return(
-        <header className='header'>
-            <div className='toolbar'> 
-                <div>
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
+            <Toolbar>
+                <img src="/images/logo.png" alt="logo" className={classes.img}/>
+                <div className={classes.grow}></div>
+                <div className={classes.userSection}>
+                    <Button variant="contained" color="primary" >
+                        Novo Post
+                    </Button>
+                    <SvgIcon className={classes.bell}>
+                       <Bell></Bell> 
+                    </SvgIcon>
+                    <Avatar alt="Remy Sharp" src="" />
+                </div>
+                {/*<div>
                     <span>Conecta Class Lock</span>
                 </div>
                 <div>
-                    <button>Novo post</button>
+                    <Button color="primary" variant="contained">Novo post</Button>
                     <span>img1</span>
                     <span>img2</span>
-                </div>
-            </div>
-         </header>
+                </div> */}
+            </Toolbar>
+        </AppBar>
     );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import Header from './components/header';
-import './style.css'
+//import './style.css'
+import { makeStyles } from '@material-ui/styles';
+
+import Header from './components/Header';
+import Feed from './components/Feed';
+import NavBar from './components/NavBar';
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    main:{
+        height: '100vh',
+        display: 'flex',
+        width: '1200px',
+        margin: '0 auto'
+    }
+})
 
 function Home() {
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
             <Header/>
-            
-            <main className='main'>
-                <div className='navbar'>
-                    navbar
-                </div>
-                <div className='feed'>
-                    feed
-                </div>
+            <main className={classes.main}>
+                <NavBar/>
+                <Feed/>
             </main>
         </div>
     );
